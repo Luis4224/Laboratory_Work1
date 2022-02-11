acc15aa665970e69ece7da82bb658adaa9862c27
Игорь Малахов <igormalahov57@gmail.com>
2022-02-12 00:06:43 +0400

    1

--- Program.cs
+++ Program.cs
@@ -4,18 +4,23 @@
     {
         static void Main()
         {
-            for(short i =0; i < 10; i++)
-            {
-                
-                if(i% 2== 0)
-                {
-                    continue;
-                }
+            Console.WriteLine("Программа для расчета по двум формулам");
+            double z1, z2;
+            Console.Write("Введите параметр а: ");
+            double a = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("El:" + i);
-            }
+
+            z1 = (Math.Sin(2 * a) + Math.Sin(5 * a) - Math.Sin(3 * a)) / (Math.Cos(a) + 1 - 2 * Math.Pow(Math.Sin(2 * a), 2));
+
+            z2 = 2 * Math.Sin(a);
+
+            Console.WriteLine("Решение по первой формуле " + z1);
+            Console.WriteLine("Решение по второй формуле " + z2);
+            
+
+
+
         }
-
     }
 
 }
