a23300e6b56e9c4cc290e32786a8a0e7ebfd9095
игорь <@LAPTOP-20N3VM1S>
2022-02-11 21:54:25 +0400

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            for(short i =0; i < 10; i++)
+            {
+                
+                if(i% 2== 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("El:" + i);
+            }
+        }
+
+    }
+
+}
